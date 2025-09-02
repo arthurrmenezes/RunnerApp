61c1d170f20f758f54ff7ce8a0322dbd5ebaa891
Arthur Menezes <arthur.menezes@ultralinks.com.br>
2025-09-01 21:04:20 -0300

    feat: Criar Name Value Object

---
+++ backend/RunnerApp.Domain/ValueObjects/FirstNameValueObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace RunnerApp.Domain.ValueObjects;
+
+public readonly struct FirstNameValueObject
+{
+    private readonly string FirstName { get; }
+
+    public const int MinLength = 3;
+    public const int MaxLength = 64;
+
+    private FirstNameValueObject(string firstName)
+    {
+        FirstName = firstName;
+    }
+
+    public static FirstNameValueObject Factory(string firstName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentNullException("First name cannot be null or empty.", nameof(firstName));
+
+        if (firstName.Length < MinLength)
+            throw new ArgumentException($"First name must be at least {MinLength} character long.", nameof(firstName));
+
+        if (firstName.Length > MaxLength)
+            throw new ArgumentException($"First name cannot exceed {MaxLength} characters.", nameof(firstName));
+
+        string formattedFirstName = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(firstName.ToLowerInvariant());
+
+        return new FirstNameValueObject(formattedFirstName);
+    }
+
+    public override string ToString() => FirstName;
+
+    public static implicit operator string(FirstNameValueObject firstName) => firstName.FirstName;
+    public static implicit operator FirstNameValueObject(string firstName) => Factory(firstName);
+}
---
+++ backend/RunnerApp.Domain/ValueObjects/SurnameValueObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace RunnerApp.Domain.ValueObjects;
+
+public readonly struct SurnameValueObject
+{
+    private string Surname { get; }
+
+    public const int MinLength = 3;
+    public const int MaxLength = 64;
+
+    private SurnameValueObject(string surname)
+    {
+        Surname = surname;
+    }
+
+    public static SurnameValueObject Factory(string surname)
+    {
+        if (string.IsNullOrWhiteSpace(surname))
+            throw new ArgumentException("Surname cannot be null or empty.", nameof(surname));
+
+        if (surname.Length < MinLength)
+            throw new ArgumentException($"First name must be at least {MinLength} character long.", nameof(surname));
+
+        if (surname.Length > MaxLength)
+            throw new ArgumentException($"Surname name cannot exceed {MaxLength} characters.", nameof(surname));
+
+        string trimmedSurname = surname.Trim();
+        string formattedSurnameName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(trimmedSurname.ToLower());
+
+        return new SurnameValueObject(formattedSurnameName);
+    }
+
+    public string GetSurname()
+    {
+        return Surname;
+    }
+
+    public override string ToString() => Surname;
+
+    public static implicit operator string(SurnameValueObject surname) => surname.GetSurname();
+    public static implicit operator SurnameValueObject(string surname) => Factory(surname);
+}
